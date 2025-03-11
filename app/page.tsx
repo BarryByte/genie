e628014dcd3e1db7f37da1c28e17f3242d2ab40c
BarryByte <abhayraj.12667@gmail.com>
2025-03-11 09:52:31 +0530

    add next-provider and shadcn for dark themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      
+      <h2>this is my now </h2>
+      <Button>Subscribe</Button>
     </div>
   );
 }
